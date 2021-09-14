170893230575c47f14784e9b74d9c7701b0d2be5
Jocoboy <Jocoboy@outlook.com>
2021-09-14 20:12:02 +0800

    clone data from url: https://www.notion.so/Wiki-7c5513c94410405baba24445cb09e131

--- web/app.js
+++ web/app.js
@@ -1,23 +1,43 @@
+var favicon = require('serve-favicon');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
+app.listen(4000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname,'/public/images/breadcrumb-3.png')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieParser());
+(function (){
+  var arr = [];
+  for(var i = 0 ; i < 10000 ; i++){
+    arr.push('keys_'+Math.random());
+  }
+  app.use(cookieSession({
+      name: 'session_id',
+      keys: arr,
+      maxAge: 20*60*1000 
+  }));
+})();
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
