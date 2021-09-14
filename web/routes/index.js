a43d366954261bd75058846fb0634e63fae0f8d3
unreal-engine-channel <unreal-engine-channel@outlook.com>
2021-09-14 20:12:02 +0800

    clone data from url: https://www.notion.so/Wiki-7c5513c94410405baba24445cb09e131

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('gujian_ol/index', { title: '古剑奇谭网络版Wiki' });
 });
 
 module.exports = router;
